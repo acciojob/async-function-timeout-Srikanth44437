4eb32605b9a1290fc79915bceaa742206b4f00a4
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-02-21 14:31:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+//your JS code here. If req
+document.addEventListener('DOMContentLoaded', () => {
+	const btn = document.getElementById("btn");
+
+	btn.addEventListener('click', handleButtonClick);
+});
+
+async function handleButtonClick() {
+	const textInput = document.getElementById("text");
+	const delayInput = document.getElementById("delay");
+	const outputInput = document.getElementById("output");
+
+	const text = textInput.value;
+	const delay = parseInt(delayInput.value);
+
+	 outputInput.innerText = '';
+
+	 try {
+        await delayAsync(delay);
+        outputInput.innerText = text;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function delayAsync(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
